1b1e94c4a5409ed3b404cc1de11e37be036b0dbe
Talha Sadeeq <talhasadiqpro21401@gmail.com>
2026-09-15 23:57:54 +0500

    Configure Vercel WSGI routing directly to app.py

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add root directory to python path for imports
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import create_app
-
-app = create_app()
